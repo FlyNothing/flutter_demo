08441dc9bbee20bc89977a9469ae64cc58a54b39
young <young@qq.com>
2022-10-12 12:05:19 +0800

    [A] - 新增device_preview插件

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/common/home/my_app.dart';
+import 'package:flutter_demo/plugin/devicepreview/device_preview_demo.dart';
 import 'package:url_strategy/url_strategy.dart';
 
 void main() {
   setPathUrlStrategy();
-  runApp(const MyApp());
+  runApp(isDevicePreview() ? getDevicePreview() : const MyApp());
 }
