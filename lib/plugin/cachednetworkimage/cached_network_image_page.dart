98d68f0026cc92576c1116b34f00e760b5363375
young <young@qq.com>
2022-11-02 16:27:42 +0800

    [U] - cached_network_image图片地址从输入框获取

--- lib/plugin/cachednetworkimage/cached_network_image_page.dart
+++ lib/plugin/cachednetworkimage/cached_network_image_page.dart
@@ -1,9 +1,18 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/common/util/global_widget.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class CachedNetworkImagePage extends StatelessWidget {
-  const CachedNetworkImagePage({super.key});
+class CachedNetworkImagePage extends StatefulWidget {
+  const CachedNetworkImagePage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _CachedNetworkImagePageState();
+}
+
+class _CachedNetworkImagePageState extends State<CachedNetworkImagePage> {
+  final TextEditingController _controller = TextEditingController();
+  String _imageUrl = "";
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +22,25 @@ class CachedNetworkImagePage extends StatelessWidget {
     );
   }
 
-  Widget _getBody(BuildContext context) {
+  _getBody(BuildContext context) {
+    return Column(
+      children: [
+        _getUrlTextField(),
+        textButtonStandard("刷新图片", () {
+          _imageUrl = _controller.text;
+          setState(() {
+            debugPrint("刷新图片");
+          });
+        }),
+        _getImage(),
+      ],
+    );
+  }
+
+  Center _getImage() {
     return Center(
       child: CachedNetworkImage(
-        imageUrl: "https://upload.jianshu.io/users/upload_avatars/24076073/c9db8271-5b04-4634-bff5-4eb690ca641c.jpg",
+        imageUrl: _imageUrl,
         // imageBuilder: (context, imageProvider) => Container(
         //   decoration: BoxDecoration(
         //     image: DecorationImage(image: imageProvider, fit: BoxFit.cover, colorFilter: const ColorFilter.mode(Colors.red, BlendMode.colorBurn)),
@@ -26,6 +50,15 @@ class CachedNetworkImagePage extends StatelessWidget {
         progressIndicatorBuilder: (context, url, downloadProgress) => CircularProgressIndicator(value: downloadProgress.progress),
         errorWidget: (context, url, error) => const Icon(Icons.error),
       ),
+    );
+  }
+
+  Container _getUrlTextField() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.w),
+      height: 40.h,
+      width: 1.sw,
+      child: textFieldStandard(_controller, hintText: "请输入图片下载地址"),
     );
   }
 }
