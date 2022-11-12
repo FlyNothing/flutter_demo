055a3800029475d878b5fa6413fe2cef8acc9b5e
young <young@qq.com>
2022-11-12 15:06:36 +0800

    [U] - Navigator示例调整

--- lib/base/router/router_page.dart
+++ lib/base/router/router_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/base/router/router2_page.dart';
 import 'package:flutter_demo/common/util/global_widget.dart';
 import 'package:flutter_demo/common/util/text_style.dart';
 
@@ -22,12 +23,22 @@ class RouterPage extends StatelessWidget {
       argu = argMap;
     }
 
-    return Center(
-      child: Container(
-        alignment: Alignment.center,
-        decoration: BoxDecoration(border: Border.all(width: 1, color: Colors.blue)),
-        child: Text("This is a router page. argMap = $argu", textAlign: TextAlign.center, style: size16W500(color: Colors.blue)),
+    return GestureDetector(
+      child: Center(
+        child: Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(border: Border.all(width: 1, color: Colors.blue)),
+          child: Text("This is a router page. argMap = $argu", textAlign: TextAlign.center, style: size16W500(color: Colors.blue)),
+        ),
       ),
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          settings: const RouteSettings(arguments: {"page-title": "router2"}),
+          builder: (context) => const Router2Page(),
+        ),
+      ),
+      // Navigator.pushNamed(context, Router2Page.routeName, arguments: {"page-title": "router2"}),
     );
   }
 }
